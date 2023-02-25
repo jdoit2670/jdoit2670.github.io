2ad006c7e4f300e8f5cb1848a70dbbbc5d5b25d8
jdoit <jdoit2670@gmail.com>
2023-02-25 15:04:20 +0530

    4

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component() {
   const element = document.createElement("div");
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(["testing", "12345678"], " ");
+  element.innerHTML = _.join(["testing234", "12345678"], " ");
   element.classList.add("hello");
 
   return element;

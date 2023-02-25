5b26642e319ab6e4b2c95dca50240ac24cc7690a
jdoit <jdoit2670@gmail.com>
2023-02-25 13:29:03 +0530

    build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, "build"),
   },
   module: {
     rules: [
